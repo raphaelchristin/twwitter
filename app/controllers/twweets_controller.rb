e01168f5b6f22872df849a8d5dc6f2d6a0024bf6
Raphaël Christin <raphael.christin@mail.mcgill.ca>
2022-05-26 16:13:45 -0400

    style the devise forms

--- app/controllers/twweets_controller.rb
+++ app/controllers/twweets_controller.rb
@@ -1,5 +1,6 @@
 class TwweetsController < ApplicationController
   before_action :set_twweet, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /twweets or /twweets.json
   def index
